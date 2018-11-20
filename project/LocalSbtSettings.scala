0a2b677a176b3704edc3eae80c4e437780a3c1fd
Andrew Gibson <andrew@andrewresearch.net>
2018-11-20 15:23:00 +1000

    tap-201 PosStats Batch

--- project/LocalSbtSettings.scala
+++ project/LocalSbtSettings.scala
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2016-2018 original author or authors
  *
@@ -12,7 +13,7 @@
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  *
- */ */
+ */
 
 /**
   * Created by andrew@andrewresearch.net on 7/11/17.
